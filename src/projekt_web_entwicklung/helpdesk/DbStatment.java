0dd81a23f60a09de7aaa540ded2de9baade7bcf7
BobTranferNotebook <BobTranferNotebook@10.20.5.224>
2019-06-11 13:47:47 +0200

    Daten an die Oberfläche transportieren für Anfrage.

--- src/projekt_web_entwicklung/helpdesk/DbStatment.java
+++ src/projekt_web_entwicklung/helpdesk/DbStatment.java
@@ -8,68 +8,143 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 import javax.inject.Named;
 
 import projekt_web_entwicklung.helpdesk.Util;
 
 @Named("DbStatment")
 @SessionScoped
-public class DbStatment implements  Serializable {
-	private  static  final  long  serialVersionUID  =  1L;
-	
-	/*  Util  ist  eine  Hilfsklasse,  die  u.  a.  den  Verbindungsaufbau  zur  Datenbank
-	*  vereinfacht:  */
-	
-	private  Util  util  =  new  Util();
+public class DbStatment implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	private  Connection  con  =  null;
-	private  Statement  stm  =  null;
-	private  ResultSet  rs  =  null;
-	
-	/**
-	*  Datensatz  einfügen
-	*  @param  ae  ActionEvent 
-*/
-	public  void  insert_ticket( int tNr, int status, int anfrage, int rechner, int kategorie, int user, String grund, String bemerkung )  {
+	/*
+	 * Util ist eine Hilfsklasse, die u. a. den Verbindungsaufbau zur Datenbank
+	 * vereinfacht:
+	 */
 
-		try {
-			//if(  ps  ==  null  ){
-			String  sQl  =  "INSERT  INTO  ticket(  "  +
-			"TicketNr,PersNr_FK, Rechner_FK, Status_FK, Bemerkung, Kategorie, Problem, Anfrage)  "  +
-			"VALUES  (  ?,  ?,  ?,  ?,  ?, ?, ?, ? )";
-				PreparedStatement  ps  =  con.prepareStatement(  sQl  );
-			//}
+	private Util util = new Util();
+
+	private Connection con = null;
+	private Statement stm = null;
+	private ResultSet rs = null;
+	private  boolean  connected  =  false;
+	
+
+public  void  connect()  { 
 		
-			ps.setInt		(1,  tNr);
-			ps.setInt  		(2,  status);
-			ps.setInt  		(3,  anfrage);
-			ps.setInt		(4,  rechner);
-			ps.setInt 		(5,  kategorie); 
-			ps.setInt  	  	(6,  user);
-			ps.setString  	(7,  grund);
-			ps.setString  	(8,  bemerkung);
-			
+		//out.println(  "connect()..."  ); 
 		
-			int  n  =  ps.executeUpdate(); 
-			if( n == 1 ) {
-				out.println(  "O.K.,	Datensatz  eingefügt.");
+		if(  util  !=  null  )  con  =  util.getCon();
+		if(  con  !=  null  ) connected  =  true;
+		else  {
+			FacesContext.getCurrentInstance().addMessage(  null,  new  FacesMessage(
+					FacesMessage.SEVERITY_ERROR,  "Exception",
+					"Keine  Verbindung  zur  Datenbank  (Treiber  nicht  gefunden?)"  ));
+			out.println(  "Keine  Verbingung  zur  Datenbank"  );
+		}
+	} 
+
+	public  void  disconnect()  { 
+		
+		if(  con  !=  null  )  {
+			try {
+				if(  rs	!=  null  )  rs.close();
+				if(  stm	!=  null  )  stm.close(); 
+		
+				util.closeConnection(  con  );
+				connected  =  false;
+			}  catch(  Exception  ex  )  {
 				FacesContext.getCurrentInstance().addMessage(  null,  new  FacesMessage(
-				FacesMessage.SEVERITY_INFO,  "O.  K.",
-						"Ein  Datensatz  erfolgreich  eingefügt."  )
-						);
+				FacesMessage.SEVERITY_ERROR,  "Exception",  ex.getLocalizedMessage())
+				);
+				out.println(  "Error:  "  +  ex  );
+				ex.printStackTrace();
 			}
-			ps.close();
-	
-		}catch(  SQLException  ex  )  {
+		}
+	}
+	/*
+	 * Lese aus der Datenbank die Tabelle Anfrage
+	 */
+	public  List<SelectItem> select_Anfrage () {
+		final  String  SQL_SELECT  =  "select  mAnfrageNr" + 
+											  "Name" + 
+											  "Beschreibung  "  +
+											  "from  anfrage";
+		
+		List<SelectItem> z = new ArrayList<SelectItem>();
+		
+		try {
+			stm  =  con.createStatement(  ResultSet.TYPE_SCROLL_SENSITIVE,
+					ResultSet.CONCUR_UPDATABLE  );
+			rs  =  stm.executeQuery(  SQL_SELECT  );
+			
+			while(rs.next()) {
+				z.add( new SelectItem( rs.getString(1), rs.getString(2),rs.getString(3)));
+			}
+			return z;
+			
+		}catch(  Exception  ex  )  {
 			FacesContext.getCurrentInstance().addMessage(  null,  new  FacesMessage(
 					FacesMessage.SEVERITY_ERROR,  "SQLException",  ex.getLocalizedMessage())
 					);
 			out.println(  "Error:  "  +  ex  );
 			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	/* 
+	 * Ticketdatensatz normalisiert einfügen
+	 */
+	public  void  insert_ticket( int tNr, int status, int anfrage, int rechner, int kategorie, int user, String grund, String bemerkung, Date startDate,Date endDate )  {
+
+		if (connected) {
+			try {
+				//if(  ps  ==  null  ){
+				String  sQl  =  "INSERT  INTO  ticket(  "  +
+				"TicketNr,PersNr_FK, Rechner_FK, Status_FK, Bemerkung, Kategorie, Problem, Anfrage,StartDate, EndDate)  "  +
+				"VALUES  (  ?,  ?,  ?,  ?,  ?, ?, ?, ?, ?, ? )";
+					PreparedStatement  ps  =  con.prepareStatement(  sQl  );
+				//}
+			
+				ps.setInt		(1,  tNr);
+				ps.setInt  		(2,  status);
+				ps.setInt  		(3,  anfrage);
+				ps.setInt		(4,  rechner);
+				ps.setInt 		(5,  kategorie); 
+				ps.setInt  	  	(6,  user);
+				ps.setString  	(7,  grund);
+				ps.setString  	(8,  bemerkung);
+				ps.setDate		(9, (java.sql.Date) startDate);
+				ps.setDate		(10, (java.sql.Date) endDate);
+				
+			
+				int  n  =  ps.executeUpdate(); 
+				if( n == 1 ) {
+					out.println(  "O.K.,	Datensatz  eingefügt.");
+					FacesContext.getCurrentInstance().addMessage(  null,  new  FacesMessage(
+					FacesMessage.SEVERITY_INFO,  "O.  K.",
+							"Ein  Datensatz  erfolgreich  eingefügt."  )
+							);
+				}
+				ps.close();
+		
+			}catch(  SQLException  ex  )  {
+				FacesContext.getCurrentInstance().addMessage(  null,  new  FacesMessage(
+						FacesMessage.SEVERITY_ERROR,  "SQLException",  ex.getLocalizedMessage())
+						);
+				out.println(  "Error:  "  +  ex  );
+				ex.printStackTrace();
+			}
 		}
 	}
 }
--- src/projekt_web_entwicklung/helpdesk/Helpdesk.java
+++ src/projekt_web_entwicklung/helpdesk/Helpdesk.java
@@ -2,26 +2,26 @@ package projekt_web_entwicklung.helpdesk;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
+import javax.faces.model.SelectItem;
 
 import projekt_web_entwicklung.helpdesk.DbStatment;
 
 @Named("Helpdesk")
 @SessionScoped
 public class Helpdesk implements Serializable  {
-	private static final long serialVersionUID = 1L;
 	
-	public Helpdesk() {
-        System.out.println( "MyBean.<init>..."  );
-        System.out.println( (new Date()).toString() ); 
-    }
+	private static final long serialVersionUID = 1L;
 	private  DbStatment  statment  =  new  DbStatment();
+	
 	private int tNr;
 	private int status;
 	private int anfrage;
@@ -30,7 +30,20 @@ public class Helpdesk implements Serializable  {
 	private int user;
 	private String grund; 
 	private String bemerkung;
-       
+	private Date startdate;
+	private Date enddate;
+	private List<SelectItem> options = new ArrayList<SelectItem>();
+	
+	public Helpdesk() {
+        System.out.println( "MyBean.<init>..."  );
+        System.out.println( (new Date()).toString() ); 
+        options = statment.select_Anfrage();
+    }
+	
+	public List getOptions() {
+		return options;
+	}
+	
     public int gettNr() {
 		return tNr;
 	}
@@ -94,9 +107,26 @@ public class Helpdesk implements Serializable  {
 	public void setBemerkung(String bemerkung) {
 		this.bemerkung = bemerkung;
 	}
+	
+	public Date getStartDate() {
+		return startdate;
+	}
+    
+	public void setStartDate(Date startdate) {
+		this.startdate = startdate;
+	}
+	
+	public Date getEndDate() {
+		return enddate;
+	}
+    
+	public void setEndDate(Date enddate) {
+		this.enddate = enddate;
+	}
+	
 	public void insertTicket(ActionEvent ae) {
 		try{
-			statment.insert_ticket(tNr, status, anfrage, rechner, kategorie, user, grund, bemerkung);
+			statment.insert_ticket(tNr, status, anfrage, rechner, kategorie, user, grund, bemerkung, startdate, enddate);
 		}catch(Exception e){
 			FacesContext.getCurrentInstance().addMessage( null,
 					 new FacesMessage(FacesMessage.SEVERITY_ERROR,"Fehler.","Die Es wurde die Methode abgebrochen."));
